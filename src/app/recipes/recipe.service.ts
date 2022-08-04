b0cc129f6def3d904f77cb6e11c6915891359ac1
Santiago Calderón <andres.calderon@kinandcarta.com>
2022-08-04 10:53:00 -0500

    Forms updaget

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,11 +4,12 @@ import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>;
 
   private recipes: Recipe[] = [
     new Recipe(
       'Juicy Burguer', 
-      'This is simply a test', 
+      'Yummy', 
       'https://i.pinimg.com/736x/96/1d/74/961d74148ad5ab689c75d84c19dc8897.jpg',
       [
         new Ingredient('Meat',1),
@@ -16,7 +17,7 @@ export class RecipeService {
       ]),
       new Recipe(
         'I dont know wth is that', 
-        'This is simply a test', 
+        'Some description xd', 
         'https://live.staticflickr.com/65535/51129289518_94009afae3_b.jpg',
         [
           new Ingredient('Bread',2),
@@ -32,5 +33,18 @@ export class RecipeService {
     return this.recipes.slice()[index];
   }
 
+  addRecipe(recipe: Recipe){
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+  
+  updateRecipe(index: number, newRecipe: Recipe){
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
+  deleteRecipe(index: number){
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -4,6 +4,7 @@ import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService{
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>(); 
 
   ingredients: Ingredient[] = [
     new Ingredient('Apples',5),
@@ -22,5 +23,19 @@ export class ShoppingListService{
 
   getIngredients(){
     return this.ingredients.slice();
+  }
+
+  getIngredient(index: number){
+    return this.ingredients[index];
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient){
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index){
+    this.ingredients.splice(index,1);
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
